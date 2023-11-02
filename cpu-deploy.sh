ec97b947573dff5a54552a4f9b5149efcac339e6
Viraj Shah <79002760+viraj-s15@users.noreply.github.com>
2023-11-02 13:38:36 +0530

    added cpu deployment

---
+++ cpu-deploy.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Function to display usage instructions
+usage() {
+  echo "Usage: $0 [test-run] [scale-replicas]"
+  echo "If 'test-run' is provided, the script will run the Docker image for testing."
+  echo "If 'scale-replicas' is provided, the script will scale the deployment to the specified number of replicas at the end."
+  exit 1
+}
+
+# Check if 'test-run' argument is provided
+if [ "$1" == "test-run" ]; then
+  run_docker=true
+  echo "Testing the Docker image will be executed after build."
+else
+  run_docker=false
+  echo "Testing the Docker image will be skipped."
+fi
+
+# Navigate to the directory containing your Dockerfile
+cd cpu/Pip
+
+# Build the Docker image
+echo "Building the Docker image..."
+docker build -t bert-inference:v1 .
+echo "Docker image built successfully."
+
+# Run the Docker image for testing if requested
+if [ "$run_docker" = true ]; then
+  echo "Running the Docker image for testing..."
+  docker run -it --rm -P bert-inference:v1
+  echo "Docker image test complete."
+fi
+
+# Start Minikube
+echo "Starting Minikube..."
+minikube start
+echo "Minikube started successfully."
+
+# Load the Docker image into Minikube
+echo "Loading the Docker image into Minikube..."
+minikube image load bert-inference:v1
+echo "Docker image loaded into Minikube."
+
+# Confirm that the image has been loaded into Minikube
+echo "List of images loaded in Minikube:"
+minikube image list
+
+# Create a Kubernetes deployment using kubectl
+echo "Creating a Kubernetes deployment..."
+kubectl create deploy bert-inference-deployment --image=bert-inference:v1
+echo "Kubernetes deployment created."
+
+# Check the list of deployments
+echo "List of Kubernetes deployments:"
+kubectl get deploy
+
+# Check the list of pods
+echo "List of pods in the deployment:"
+kubectl get pod
+
+# Expose the deployment as a service with specified ports
+echo "Exposing the deployment as a service..."
+kubectl expose deploy/bert-inference-deployment --name=bert-inference-deployment --target-port=8888 --port=1234
+echo "Deployment exposed as a service."
+
+# Access the service using Minikube
+echo "Accessing the service using Minikube..."
+minikube service bert-inference-deployment
+
+# Check if 'scale-replicas' argument is provided
+if [ -n "$2" ]; then
+  # Scale the deployment to the specified number of replicas
+  echo "Scaling the deployment to $2 replicas..."
+  kubectl scale deploy/bert-inference-deployment --replicas="$2"
+  echo "Deployment scaled to $2 replicas."
+fi
